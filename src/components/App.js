48e8c10a3ce3d1c73eb9f35f62cb096907dcbab5
nakatahr <ep3may20@gmail.com>
2021-01-20 20:48:40 +0900

    apply grid layout

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,12 @@ import React, { useState } from 'react';
 
 import Main from './Main.js';
 import SignIn from './SignIn';
+import config from '../config.json';
 
 export default () => {
   const [nickname, setNickname] = useState('');
-  console.log({ nickname });
 
-  if (nickname === ''){
+  if (config.singInEnabled && nickname === '' ){
     return <SignIn setNickname={setNickname} />;
   } else {
     return <Main nickname={nickname} />;
---
+++ src/components/MessageInputField.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const MessageInputField = () => {
+    return <div>Message Input Field</div>
+};
+
+export default MessageInputField;
