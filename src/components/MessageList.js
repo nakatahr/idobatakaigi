e18b73369a40c4b03e9b5ba88034d1e2f0ba2412
Hiroaki Nakata <ep3may20@gmail.com>
2021-02-13 19:24:13 +0900

    retrieve messages from database

--- src/components/MessageList.js
+++ src/components/MessageList.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+
+import { messagesRef } from '../firebase';
 
 const useStyles = makeStyles({
     root: {
@@ -8,7 +10,27 @@ const useStyles = makeStyles({
 });
 
 const MessageList = () => {
+    const [messages, setMessages] = useState([]);
     const classes = useStyles();
+
+    useEffect(() => {
+        messagesRef
+        .orderByKey()
+        .limitToLast(3)
+        .on("value", (snapshot) => {
+            const messages = snapshot.val();
+            if (messages === null) return;
+            const entries = Object.entries(messages);
+            const newMessages = entries.map((e) => {
+                const [key, nameAndText] = e;
+                return {key, ...nameAndText}
+            });
+            console.log(newMessages);
+            setMessages(newMessages);
+        }, function (errorObject) {
+            console.log("The read failed: " + errorObject.code);
+        });
+    }, []);
     return <div className={classes.root}>Message List</div>
 };
 
